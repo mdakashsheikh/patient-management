8c1180452632fbb43107deff8d393fb671705640
MdAkashSheikh <nmakashsheikh@gmail.com>
2024-08-28 05:13:17 +0600

    patient management

---
+++ app/page.tsx
@@ -0,0 +1,23 @@
+import { Button } from '@/components/ui/button'
+import Image from 'next/image'
+import React from 'react'
+
+const Home = () => {
+    return (
+        <div className='flex h-screen max-h-screen'>
+            <section className='remove-scrollbar container my-auto'>
+                <div className='sub-container max-w-[496px]'>
+                    <Image 
+                        src="/assets/icons/logo-full.svg"
+                        width={1000}
+                        height={1000}
+                        alt='patient'
+                        className='mb-12 h-10 w-fit'
+                    />
+                </div>
+            </section>
+        </div>
+    )
+}
+
+export default Home
