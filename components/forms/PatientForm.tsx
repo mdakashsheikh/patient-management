056a9abf3d3f784f3cda47a85815205d122d4f03
MdAkashSheikh <nmakashsheikh@gmail.com>
2024-08-29 08:10:56 +0600

    add database and app-write settings

--- components/forms/PatientForm.tsx
+++ components/forms/PatientForm.tsx
@@ -10,6 +10,7 @@ import SubmitButton from "../SubmitButton"
 import { useState } from "react"
 import { UserFormValidation } from "@/lib/validation"
 import { useRouter } from "next/navigation"
+import { createUser } from "@/lib/actions/patient.actions"
 
 export enum FormFieldType {
     INPUT = 'input',
@@ -39,15 +40,21 @@ export const PatientForm = () => {
         setIsLoading(true)
 
         try {
-            // const userData = { name, email, phone };
+            const userData = { name, email, phone };
 
-            // const user = await createUser(userData)
+            console.log('userData--', userData)
 
-            // if(user) router.push(`/patient/${user.$id}/register`)
+            const user = await createUser(userData)
+
+            console.log("User-->", user)
+
+            if(user) router.push(`/patient/${user.$id}/register`)
             
         } catch (error) {
             console.log(error)
         }
+
+        setIsLoading(false)
     }
     return (
         <Form {...form}>
